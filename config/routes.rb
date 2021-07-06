18ae120e3bab690f47c7ab78afa7b7790fd88824
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-07-06 22:34:34 +0200

    Create form for new session view and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
   root "users#index"
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   get 'cakes/index'
+
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
