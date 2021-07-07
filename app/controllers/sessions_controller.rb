231f93813206257dce2c8358b0c7bd3c9644db1c
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-07-07 12:55:45 +0200

    Update controllers and links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_name(params[:name])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id 
-      redirect_to root_url, notice: "Logged in!"
+      redirect_to user, notice: "Logged in!"
     else
       flash.now[:alert] = "Email or password is invalid"
       render "new"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
