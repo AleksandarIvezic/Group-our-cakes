88906aaae1ca17900538cf466f8f58e04839e6e7
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-07-07 11:43:27 +0200

    Add navbar and some styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :authenticate_with_http_digest
 
   # GET /users or /users.json
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :cakes, foreign_key :author_id
+  has_many :cakes, foreign_key: :author_id
   has_many :groups
 
   validates :name, presence: true, uniqueness: true
