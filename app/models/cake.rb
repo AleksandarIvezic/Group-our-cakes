4503cb7c314beab37a6c3c483ec158382e8bdb49
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-07-08 12:41:22 +0200

    Update cake and group associations

--- app/models/cake.rb
+++ app/models/cake.rb
@@ -1,5 +1,6 @@
 class Cake < ApplicationRecord
   has_and_belongs_to_many :groups
+  belongs_to :author, class_name: "User"
 
   validates :name, presence: true 
   validates :amount, presence: true
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
-  has_and_belongs_to_many :users
+  belongs_to :users
   has_and_belongs_to_many :cakes
-
+  
   validates :name, presence: true, uniqueness: true
   validates :icon, presence: true
 
