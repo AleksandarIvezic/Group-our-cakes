9aa25010564ed91c7a22c82d62a85aaf459b436b
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-07-12 17:46:34 +0200

    Add create group test

---
+++ spec/features/create_group_spec.rb
@@ -0,0 +1,27 @@
+describe 'Create category', type: :feature do
+    before :each do
+      @user = User.create(name: 'username', password: 'password', avatar: 'app/helpers/test_helper/img_avatar.png')
+      visit '/login'
+      fill_in 'Name', with: 'username'
+      fill_in 'Password', with: 'password'
+      click_button 'Login'
+    end
+    after :each do
+      @user.destroy
+    end
+  
+    it 'Create new item' do
+        visit '/groups/new'
+        fill_in 'Name', with: 'group_name'
+        attach_file('Icon', 'app/helpers/test_helper/img_avatar.png')
+        click_button 'Create Group'
+      expect(page).to have_content('Category created successfully!')
+    end
+    it "Require fields filled up" do
+      visit '/groups/new'
+      fill_in 'Name', with: ''
+      attach_file('Icon', 'app/helpers/test_helper/img_avatar.png')
+      click_button 'Create Group'
+      expect(page).to have_content 'Please fill required fields!'
+    end
+  end
