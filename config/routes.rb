b56393990e6f3a58880ac9560b29e8534b65d932
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-07-13 11:15:43 +0200

    Remove comented code from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'groups/index'
-  # get 'sessions/new'
-  # get 'sessions/create'
-  # get 'sessions/destroy'
   root "home#index"
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
@@ -17,5 +14,4 @@ Rails.application.routes.draw do
   post "add_to_group", to: "cakes#add_to_group", as: "add_to_group"
   get "remove_group", to: "cakes#remove_group", as: "remove_group"
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
