670c343d521ffc6f9d5e91b03e5ac1df13779f77
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-07-13 11:00:21 +0200

    Add default img for uncategorized items

--- app/helpers/cakes_helper.rb
+++ app/helpers/cakes_helper.rb
@@ -6,4 +6,12 @@ module CakesHelper
   def desc_dot
     content_tag :div, '', class: 'dot' if params[:order] == 'desc'
   end
+
+  def category_img(cake)
+    if cake.groups.first
+      cake.groups.first.icon
+    else
+      "uncategorized/uncategorized.jpeg"
+    end
+  end
 end
